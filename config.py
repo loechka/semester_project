e9a34dbd24b21d64e1227c3cf1f017c47265a0cb
loechka <anuta.anna.ann@yandex.ru>
2021-05-06 16:35:29 +0800

    added all configurations

---
+++ config.py
@@ -0,0 +1,39 @@
+
+screen_width = 800
+screen_height = 600
+# background_image = 'images/background.jpg'
+
+# frame_rate = 90
+frame_rate = 90
+
+status_offset_y = 5
+
+text_color = (255, 255, 0)
+initial_lives = 3
+lives_right_offset = 85
+lives_offset = screen_width - lives_right_offset
+score_offset = 5
+
+font_name = 'Arial'
+font_size = 20
+
+effect_duration = 20
+
+# sounds_effects = dict(
+#     brick_hit='sound_effects/brick_hit.wav',
+#     effect_done='sound_effects/effect_done.wav',
+#     paddle_hit='sound_effects/paddle_hit.wav',
+#     level_complete='sound_effects/level_complete.wav',
+# )
+
+message_duration = 2
+
+button_text_color = (255, 255, 255),
+button_normal_back_color = (0,139,139)
+button_hover_back_color = (238, 99, 99)
+button_pressed_back_color = (205, 85, 85)
+
+menu_offset_x = 400
+menu_offset_y = 300
+menu_button_w = 200
+menu_button_h = 50
