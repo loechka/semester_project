bd8d07d6a9e551fd67767a8d32b7a255aa8fcc3b
Helen <lena.gorbatova1@gmail.com>
2021-06-02 01:11:44 +0300

    Add docstrings for Bonus class

--- bonus.py
+++ bonus.py
@@ -1,11 +1,41 @@
-import pygame
+"""Module contains Bonus class."""
 
+import pygame
 from game_object import GameObject
 import config as c
 
 
 class Bonus(GameObject):
-    def __init__(self, x, y, w, h, color, speed, good=True, type=0, seen=True):
+    """Bonus object class, based on GameObject class.
+
+    There are two types of bonuses: good - star, bad - bomb.
+    Every good or bad bonus can have two different effects: change amount of lives, change charecter size.
+    """
+
+    def __init__(
+                self, x: int, 
+                y: int, 
+                w: int, 
+                h: int, 
+                color: tuple, 
+                speed: int, 
+                good: bool=True, 
+                type: int=0, 
+                seen: bool=True):
+        """
+        Init Bonus with certain features.
+
+        Keyword arguments:
+        :param x: left coordinate
+        :param y: top coordinate
+        :param w: object width
+        :param h: object height
+        :param color: object color
+        :params peed: object speed
+        :param good: is bonus good - True - star, False - bomb (default True)
+        :param type: bonus type: 0 - +/- lives, 1 - change size (default 0)
+        :param seen: is bonus seen (default True)
+        """
         GameObject.__init__(self, x, y, w, h, speed)
         self.good = good
         self.type = type
@@ -13,7 +43,13 @@ class Bonus(GameObject):
         self.color = color
         self.load_image(self.good)
 
-    def load_image(self, good):
+    def load_image(self, good: bool):
+        """
+        Load certain image according to bonus type.
+
+        Keyword arguments:
+        :param good: is bonus good: True - star, False - bomb (default True)
+        """
         if good:
             self.filepath = c.star_image
         else:
@@ -24,13 +60,24 @@ class Bonus(GameObject):
                                     (self.bounds.width, self.bounds.height))
 
     def draw(self, surface):
+        """
+        Draw bonus.
+
+        Keyword arguments:
+        :param surface: pygame Surface
+        """
         if self.seen:
             surface.blit(self.myImage, self.bounds)
 
     def update(self):
+        """Update bonus."""
         super().update()
 
     def delete(self):
+        """Delete bonus.
+
+        Makes it an invisible dot.
+        """
         self.seen = False
         self.bounds.height = 0
         self.bounds.width = 0
