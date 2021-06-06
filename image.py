24bd0903c709943735c7adcda65bc2212badf80b
loechka <anuta.anna.ann@yandex.ru>
2021-06-06 23:03:07 +0300

    delete comments

--- image.py
+++ image.py
@@ -15,12 +15,9 @@ class Image(GameObject):
         self.seen = seen
         self.myImage = pygame.image.load(file_path)
         self.myImage = pygame.transform.scale(self.myImage, (w, h))
-        # self.rect = self.myImage.get_rect()
         self.coordinates = (x, y)
 
     def draw(self, surface):
-        # pygame.draw.rect(surface, (255,0,128), self.rect, 1)
-        # pygame.display.update()
         if self.seen:
             surface.blit(self.myImage, self.coordinates)
 
